c1667f2fe813b4f19db9d69b9b0f6f62a245ea48
md-ahmodullah <contactme.ahmod@gmail.com>
2024-09-23 20:10:22 +0600

    Validation is on process....

--- js/index.js
+++ js/index.js
@@ -2,18 +2,14 @@ const historyEl = getDataById('history');
 const donationEl = getDataById('donation');
 const donationBtnEl = getDataById('donation-btn');
 const historyBtnEl = getDataById('history-btn');
-const noakhaliAmount = getDataById('noakhali-amount');
-const feniAmount = getDataById('feni-amount');
-const quotaAmount = getDataById('quota-amount');
-const noakhaliTotal = getDataById('noakhali-total');
-const feniTotal = getDataById('feni-total');
-const quotaTotal = getDataById('quota-total');
-const noakhaliButton = getDataById('noakhali-button');
-const feniButton = getDataById('feni-button');
-const quotaButton = getDataById('quota-button');
-const navAmount = getDataById('nav-amount');
+const noakhaliInputAmount = getDataById('noakhali-amount');
+const feniInputAmount = getDataById('feni-amount');
+const quotaInputAmount = getDataById('quota-amount');
+const noakhaliTotalSpan = getDataById('noakhali-total');
+const feniTotalSpan = getDataById('feni-total');
+const quotaTotalSpan = getDataById('quota-total');
+const navAmountSpan = getDataById('nav-amount');
 const historySection = getDataById('history');
-const noakhaliTitle = getDataById('noakhali-title');
 const date = new Date().toString();
 
 
@@ -21,65 +17,81 @@ document.getElementById('history-btn').addEventListener('click', historyBtnFunct
 
 document.getElementById('donation-btn').addEventListener('click', donationBtnFunction);
 
-noakhaliButton.addEventListener('click', calcNoakhali);
+document.getElementById('noakhali-button').addEventListener('click', calcNoakhali);
+
+document.getElementById('feni-button').addEventListener('click', calcFeni);
+
+document.getElementById('quota-button').addEventListener('click', calcQuota);
 
 function donationBtnFunction(){
     removeProperty(donationEl, 'hidden');
-    // donationEl.classList.remove('hidden');
     addProperty(historyEl, 'hidden');
-    // historyEl.classList.add('hidden');
     addProperty(donationBtnEl, 'bg-primary');
-    // donationBtnEl.classList.add('bg-primary');
     removeProperty(historyBtnEl, 'bg-primary');
-    // historyBtnEl.classList.remove('bg-primary');
 }
 function historyBtnFunction(){
     addProperty(donationEl, 'hidden');
-    // donationEl.classList.add('hidden');
     removeProperty(historyEl, 'hidden');
-    // historyEl.classList.remove('hidden');
     removeProperty(donationBtnEl, 'bg-primary');
-    // donationBtnEl.classList.remove('bg-primary');
     addProperty(historyBtnEl, 'bg-primary');
-    // historyBtnEl.classList.add('bg-primary');
-}
-
-const noakhaliTotalNum = getDataById('noakhali-total').innerText;
-const feniTotalNum = getDataById('feni-total').innerText;
-const quotaTotalNum = getDataById('quota-total').innerText;
-const navAmountNum = getDataById('nav-amount').innerText;
-function calc(btnAmount, btnTotalNum, navTotalNum) {
-    const amount = Number(btnAmount.value);
-    const initAmount = Number(btnTotalNum);
-    const myAmount = Number(navTotalNum);
-    const myNewAmount = myAmount - amount;
-    btnAmount.innerText = myNewAmount;
-    const newAmount = initAmount + amount;
-    return newAmount;
 }
 
 function calcNoakhali() {
-    const am = calc(noakhaliAmount, noakhaliTotalNum, navAmountNum);
-    noakhaliTotal.innerText = am; 
-    showHistory();
+    const amount = Number(noakhaliInputAmount.value);
+    const initAmount = makeNumber(noakhaliTotalSpan);
+    const myAmount = makeNumber(navAmountSpan);
+    if (amount > myAmount) {
+        alert('Invalid Amount. Try again!');
+        return
+    }
+    // if (amount1 > amount2 || amount1 === '' || isNaN(amount1)) {
+    //     alert('Invalid Amount. Try again!');
+    //     return;
+    // }
+    const myNewAmount = myAmount - amount;
+    navAmountSpan.innerText = myNewAmount;
+    const newAmount = initAmount + amount;
+    noakhaliTotalSpan.innerText = newAmount;
+    noakhaliInputAmount.value = '';
+    showHistory(amount, 'noakhali-title');
 }
-// const calcFeni = calc(feniAmount, feniTotal, navAmount);
-// const calcQuota = calc(quotaAmount, quotaTotal, navAmount);
-// function calcNoakhali(event) {
-//     const amount = Number(noakhaliAmount.value);
-//     const initAmount = Number(noakhaliTotal.innerText);
-//     const myAmount = Number(navAmount.innerText);
-//     const myNewAmount = myAmount - amount;
-//     navAmount.innerText = myNewAmount;
-//     const newAmount = initAmount + amount;
-//     noakhaliTotal.innerText = newAmount;  
-// }
+function calcFeni() {
+    const amount = Number(feniInputAmount.value);
+    const initAmount = makeNumber(feniTotalSpan);
+    const myAmount = makeNumber(navAmountSpan);
+    const myNewAmount = myAmount - amount;
+    navAmountSpan.innerText = myNewAmount;
+    const newAmount = initAmount + amount;
+    feniTotalSpan.innerText = newAmount; 
+    feniInputAmount.value = '';
+    showHistory(amount, 'feni-title');
+}
+function calcQuota() {
+    const amount = Number(quotaInputAmount.value);
+    const initAmount = makeNumber(quotaTotalSpan);
+    const myAmount = makeNumber(navAmountSpan);
+    const myNewAmount = myAmount - amount;
+    navAmountSpan.innerText = myNewAmount;
+    const newAmount = initAmount + amount;
+    quotaTotalSpan.innerText = newAmount;
+    quotaInputAmount.value = '';
+    showHistory(amount, 'quota-title');
+}
+
+function blogWindow() {
+    window.location.href = "/blog.html";
+}
+function backHome() {
+    window.location.href = "/index.html";
+}
+
 
 // famine-2024 at Feni, Bangladesh
-function showHistory() {
+function showHistory(inputAmount, id) {
+    const title = titleGenerator(id)
     const history1 = `<div class="border-2 rounded-2xl p-8 space-y-4">
                 <h2 class="text-xl font-semibold text-black">
-                ${noakhaliAmount.value} Taka is Donated for ${noakhaliTitle.innerText}
+                ${inputAmount} Taka is Donated for ${title}.
                 </h2>
                 <p class="text-base font-normal text-gray-500">
                 Date : ${date}
@@ -87,7 +99,6 @@ function showHistory() {
                 </div>`
     historySection.innerHTML += history1;
 }
-
 // Re-usable Function
 function getDataById(id) {
     return document.getElementById(id);
@@ -99,3 +110,12 @@ function addProperty(element, property) {
 function removeProperty(element, property) {
     return element.classList.remove(property);
 }
+function makeNumber(el) {
+    return Number(el.innerText);
+}
+function titleGenerator(id) {
+    return getDataById(id).innerText;
+}
+    
+
+
